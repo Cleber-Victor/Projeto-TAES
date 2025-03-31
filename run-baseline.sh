b20a3f0be0853bc3f8866eb83292e455d097a4b3
= <=>
2025-03-31 15:08:20 -0300

    fix: correct list of evaluated models

--- run-baseline.sh
+++ run-baseline.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# llama-3.3-70b-versatile e llama-3.1-8b-instant já foram testados, 
-# para replicar os resultados completos, incluir os dois na lista
-
-#args=("llama3-8b-8192" "llama3-70b-8192" "llama-guard-3-8b" "gemma2-9b-it")
-
-args=("llama3-8b-8192" "llama-guard-3-8b" "gemma2-9b-it")
+args=("llama3-8b-8192" "llama3-70b-8192" "gemma2-9b-it" \
+      "llama-3.3-70b-versatile" "llama-3.1-8b-instant")
 
 for arg in "${args[@]}"; do
     python3 main.py "$arg"
